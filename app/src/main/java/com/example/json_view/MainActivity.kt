3e141744b6d0846521bde5596ee7045608a75792
Stas <Stas-Shlyckov-1988>
2023-05-30 16:34:41 +0300

    edit ip

--- app/src/main/java/com/example/json_view/MainActivity.kt
+++ app/src/main/java/com/example/json_view/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.json_view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.fasterxml.jackson.annotation.JsonProperty
@@ -30,32 +33,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        GlobalScope.launch(Dispatchers.IO) {
-            val url = URL("http://192.168.1.145/api/sur/list")
-            val httpURLConnection = url.openConnection() as HttpURLConnection
-            httpURLConnection.setRequestProperty("Accept", "application/json") // The format of response we want to get from the server
-            httpURLConnection.requestMethod = "GET"
-            httpURLConnection.doInput = true
-            httpURLConnection.doOutput = false
-            // Check if the connection is successful
-            val responseCode = httpURLConnection.responseCode
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                val response = httpURLConnection.inputStream.bufferedReader()
-                    .use { it.readText() }  // defaults to UTF-8
-                withContext(Dispatchers.Main) {
 
-                    // Convert raw JSON to pretty JSON using GSON library
-                    val gson = GsonBuilder().setPrettyPrinting().create()
-                    val prettyJson = gson.toJson(JsonParser.parseString(response))
-                    Log.d("Pretty Printed JSON :", prettyJson)
-                    val textView: TextView = findViewById(R.id.content) as TextView
-                    textView.text = prettyJson
-                }
-            } else {
-                Log.e("HTTPURLCONNECTION_ERROR", responseCode.toString())
-            }
-        }
         //getReprofit()
+        val  clickButton: Button = findViewById(R.id.button);
+        clickButton.setOnClickListener {
+            Log.d("BUTTONS", "User tapped the Supabutton")
+            GlobalScope.launch(Dispatchers.IO) {
+                val editText: EditText = findViewById(R.id.editTextText) as EditText
+                val url = URL("http://" + editText.text + "/api/sur/list")
+                val httpURLConnection = url.openConnection() as HttpURLConnection
+                httpURLConnection.setRequestProperty("Accept", "application/json") // The format of response we want to get from the server
+                httpURLConnection.requestMethod = "GET"
+                httpURLConnection.doInput = true
+                httpURLConnection.doOutput = false
+                // Check if the connection is successful
+                val responseCode = httpURLConnection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val response = httpURLConnection.inputStream.bufferedReader()
+                        .use { it.readText() }  // defaults to UTF-8
+                    withContext(Dispatchers.Main) {
+
+                        // Convert raw JSON to pretty JSON using GSON library
+                        val gson = GsonBuilder().setPrettyPrinting().create()
+                        val prettyJson = gson.toJson(JsonParser.parseString(response))
+                        Log.d("Pretty Printed JSON :", prettyJson)
+                        val textView: TextView = findViewById(R.id.content) as TextView
+                        textView.text = prettyJson
+                    }
+                } else {
+                    Log.e("HTTPURLCONNECTION_ERROR", responseCode.toString())
+                }
+            }
+        };
     }
 
 
